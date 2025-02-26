05d5787352fa4b8fb2cffaf1a96a535afe5cbf2f
panucci <eyakubu442@gmail.com>
2025-02-26 16:43:06 +0100

    adjustedRoughly

--- script.js
+++ script.js
@@ -5,3 +5,27 @@ Menu.onclick = () => {
   Menu.classList.toggle("fa-times");
   Navbar.classList.toggle("active");
 };
+
+let index = 0;
+
+function showSlide(n) {
+    let slides = document.querySelectorAll(".slide");
+    if (n >= slides.length) { index = 0; }
+    if (n < 0) { index = slides.length - 1; }
+
+    let offset = -index * 100; // Moves by 100% width
+    document.querySelector(".slider").style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+    index++;
+    showSlide(index);
+}
+
+function prevSlide() {
+    index--;
+    showSlide(index);
+}
+
+// Auto slide every 3 seconds
+setInterval(nextSlide, 3000);
